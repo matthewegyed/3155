cf142d346da59f5a6de8f745aaf1bddf2382bdc4
Matthew Egyed <mateegyed01@gmail.com>
2024-08-05 17:05:30 -0600

    initial commit

---
+++ src/test/scala/MySuite.scala
@@ -0,0 +1,10 @@
+class MySuite extends munit.FunSuite {
+  test("example test that succeeds") {
+    val obtained = 42
+    val expected = 42
+    assertEquals(obtained, expected)
+  }
+
+  //**TODO: Your tests here
+
+}
