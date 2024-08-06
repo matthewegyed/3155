7f17d486450a2c115a99ab7d7ffd2d66d83071d6
ParkerrAllen <89681695+ParkerrAllen@users.noreply.github.com>
2024-08-06 10:44:35 -0600

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,14 +21,22 @@ def eval(expr: Expr): Either[String, Value] = expr match {
   case ManyExprs(exprs) => ???
 
   case Plus(e1, e2) =>
-  (eval(e1), eval(e2)) match{
-      case (Right(VeryHappy()), _)=>Right(VeryHappy())
-      case (_, Right(VeryHappy()))=>Right(VeryHappy())
-      case (Right(Cry()), Right(v2))=>Right(v2)
-      case (Right(v1), Right(Cry())) if v1.isInstanceOf[Happy]||v1.isInstanceOf[Stun]=>Right(Cry())
-      case (Right(v1), Right(v2)) if !v2.isInstanceOf[VeryHappy]&&!v2.isInstanceOf[Cry]=>Right(v1)
-      case _ => Left("ERROR")
-  }
+      (eval(e1), eval(e2)) match{
+        case (Right(ManyVals(vs)), Right(v))=>
+          val r= vs.map{vi=>
+            eval(Plus(vi, v)) match{
+              case Right(vr)=>vr
+              case Left(_)=>return Left("ERROR")
+            }
+          }
+          Right(ManyVals(r))
+        case (Right(VeryHappy()), _)=>Right(VeryHappy())
+        case (_, Right(VeryHappy()))=>Right(VeryHappy())
+        case (Right(Cry()), Right(v2))=>Right(v2)
+        case (Right(v1), Right(Cry())) if v1.isInstanceOf[Happy]||v1.isInstanceOf[Stun]=>Right(Cry())
+        case (Right(v1), Right(v2)) if !v2.isInstanceOf[VeryHappy]&&!v2.isInstanceOf[Cry]=>Right(v1)
+        case _ => Left("ERROR")
+      }
   case Not(e) => ???
   
 }
